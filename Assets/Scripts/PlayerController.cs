50aa65fde31a66c5bf2a597d4839943a6ba2219d
Olivia Taylor <oliviataylor@Olivias-MacBook-Pro.local>
2019-02-28 13:37:42 -0500

    added score and lives text

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+
+    public int numberOfLives;
+    public Text livesText;
+
+    void Start()
+    {
+        //score.GetComponent<Text>().text = "Score: " + 20;
+        score = 0;
+        numberOfLives = 3;
+        scoreText.text = "Score: " + score.ToString ();
+        livesText.text = "Lives: " + numberOfLives.ToString();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
