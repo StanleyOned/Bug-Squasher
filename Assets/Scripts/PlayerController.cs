f7fae05f223c9e6592d1cc9d9a6887e8ea17bd7d
davidrynn <davidrynn@gmail.com>
2019-02-28 15:01:37 -0500

    Revert "added buttons to increase score and decrease lives"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,56 +11,18 @@ public class PlayerController : MonoBehaviour
     public int numberOfLives;
     public Text livesText;
 
-    public Button bugButton;
-    public Button nonBugButton;
-
     void Start()
     {
         //score.GetComponent<Text>().text = "Score: " + 20;
         score = 0;
         numberOfLives = 3;
-        setScoreText();
-        setLivesText();
+        scoreText.text = "Score: " + score.ToString ();
+        livesText.text = "Lives: " + numberOfLives.ToString();
     }
 
     // Update is called once per frame
     private void Update()
     {
         
-    }
-
-    public void addScore()
-    {
-        score += 1;
-        setScoreText();
-    }
-
-    public void loseLife()
-    {
-        if (numberOfLives > 0) {
-            numberOfLives -= 1;
-            setLivesText();
-        }
-        else
-        {
-            resetGame();
-        }
-    }
-
-    private void setScoreText()
-    {
-        scoreText.text = "Score: " + score.ToString();
-    }
-
-    private void setLivesText()
-    {
-        livesText.text = "Lives: " + numberOfLives.ToString();
-    }
-
-    private void resetGame()
-    {
-        //end game
-        //show score
-        //show menu
     }
 }
